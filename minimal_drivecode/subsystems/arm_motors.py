a1afa9b1691f19ccd86ed6e8bc6bf7d6ebc4a6b8
lollyi <lollyinman@gmail.com>
2019-02-10 13:01:37 -0800

    Fixed arm motor device numbers for testing

--- minimal_drivecode/subsystems/arm_motors.py
+++ minimal_drivecode/subsystems/arm_motors.py
@@ -9,10 +9,9 @@ class Arm_Motors():
 	
 	def __init__(self):
 		#Initialize Right motors
-		#self.arm_motor = (wpilib.VictorSP(12))
 		#self.left_arm_motor = ctre.WPI_VictorSPX(1)	
 		self.left_arm_motor = wpilib.Victor(6)
-		self.right_arm_motor = ctre.WPI_VictorSPX(01)
+		self.right_arm_motor = ctre.WPI_VictorSPX(1)
 			
 	def set_speed(self, value):
 		if value > 1 or value < -1:
