4216337a24108768e1876c1a25f0137dc2bce199
NoahBamberger <46659484+NoahBamberger@users.noreply.github.com>
2019-02-10 15:45:04 -0800

    Forward and Rearward limit switch commands
    Forward and Rearward limit switch commands

---
+++ minimal_drivecode/commands/do_forward_arm_limit.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Feb 10 13:43:06 2019
+
+@author: noahm
+"""
+from wpilib.command import Command
+
+class Do_Forward_Arm_Limit(Command):
+    def __init__(self,robot):
+        self.requires(robot.arm)
+        robot.arm.l_arm_encoder = 0
+        robot.arm.arm_motors.set_speed(0)
+    def execute():
+        pass
+    def isFinished():
+        return True
+    def end():
+        pass
+    def interrupted():
+        pass
