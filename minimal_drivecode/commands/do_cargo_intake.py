6d42eb0715445eb6f80ddce731f673aa7122f792
lollyi <lollyinman@gmail.com>
2019-02-16 13:27:14 -0800

    logical corrections to the cargo command and subsystem

--- minimal_drivecode/commands/do_cargo_intake.py
+++ minimal_drivecode/commands/do_cargo_intake.py
@@ -1,37 +1,31 @@
 # vim: set sw=4 noet ts=4 fileencoding=utf-8:
 
 import wpilib
-import wpilib.drive
 from wpilib.command import Command
+from do_move_arm import Do_Move_Arm
 		
 # Should intake and outtake cargo (bouncy ball). This should be done by
 # activating the motors for the rollers on the arm.
 class Do_Cargo_Intake(Command):
 	def __init__(self, robot):
-		
 		super().__init__()
 		
-		# instance of drivetrain
-		self.robot_dt = robot.drivetrain
+		self.requires(robot.arm)
 		self.robot_cargo = robot.cargo
-		
-		self.cargo_motor = robot.cargo.cargo_motor
-		self.left_joy = robot.left_joy
-		self.right_joy = robot.right_joy
 	
 	def initialize(self):
-		return None
-	def execute(self):
-		self.robot_cargo.cargo_intake(self.cargo_motor)
 		print("cargo intake!")
+		Do_Move_Arm(robot, 0)
+		self.robot_cargo.cargo_intake()
 
-		# Required periodical call to Differential Drive
-		self.robot_dt.set_tank_speed(
-			self.left_joy, self.right_joy, self.robot_dt.drive)
+	def execute(self):
+		pass
 	
 	def isFinished(self):
-		return None
+		return False
+
 	def end(self):
-		self.robot_cargo.cargo_reset(self.cargo_motor)
+		self.robot_cargo.cargo_reset()
+
 	def interrupted(self):
 		self.end()
--- minimal_drivecode/subsystems/cargo.py
+++ minimal_drivecode/subsystems/cargo.py
@@ -1,6 +1,5 @@
 # vim: set sw=4 noet ts=4 fileencoding=utf-8:
 
-import math
 import wpilib
 from wpilib.command import Subsystem
 
@@ -11,17 +10,14 @@ class Cargo(Subsystem):
 	def __init__(self, robot):
 		print("init")
 		super().__init__()
-		self.cargo_motor = wpilib.VictorSP(7)
+		self.cargo_motor = wpilib.VictorSP(6)
 
-	def cargo_intake(self, motor):
-		#self.set_arm_position(intake)
-		motor.set(1)
+	def cargo_intake(self):
+		self.cargo_motor.set(1)
 
-	def cargo_reset(self, motor):
-		motor.set(0)
+	def cargo_reset(self):
+		self.cargo_motor.set(0)
 			
 	# Actually ejects ball. Arm must be set correctly beforehand
-	def cargo_eject(self, motor):
-		# Cargo ball motor
-		motor.set(-1)
-
+	def cargo_eject(self):
+		self.cargo_motor.set(-1)
