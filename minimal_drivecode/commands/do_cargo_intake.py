73e4f4583e76358d5b1abce12ad5baa4206e3f78
Anastasia <meklordskiel@gmail.com>
2019-02-19 17:56:19 -0800

    fixed?

---
+++ minimal_drivecode/commands/do_cargo_intake.py
@@ -0,0 +1,37 @@
+# vim: set sw=4 noet ts=4 fileencoding=utf-8:
+
+import wpilib
+from wpilib.command import Command
+		
+# Should intake and outtake cargo (bouncy ball). This should be done by
+# activating the motors for the rollers on the arm.
+class Do_Cargo_Intake(Command):
+	def __init__(self, robot):
+		super().__init__()
+		
+		self.robot_cargo = robot.cargo
+
+		# uses motor 6
+		
+		# Cargo can only be in two states:
+		#	1: rotating inwards with Arm at front of robot
+		#	2: rotating outwards with Arm at back of robot
+
+		# state 1: possesses Cargo
+		self.requires(robot.cargo)
+	def initialize(self):
+		# intake rollers rotate inwards, intaking ball
+		print("cargo intake!")
+		self.robot_cargo.cargo_intake()
+
+	def execute(self):
+		pass
+
+	def isFinished(self):
+		return False
+
+	def end(self):
+		self.robot_cargo.cargo_reset()
+
+	def interrupted(self):
+		self.end()
